8941d53c192289523867e251a9e7e7a8b7615ca7
w <wangchh@gmail.com>
2024-05-03 11:04:39 +0800

    add language ui

---
+++ android/app/src/main/kotlin/com/llfbandit/voice_uploader/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.llfbandit.voice_uploader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flag/flag.dart';
 
 import './audio_player.dart';
 import './audio_recorder.dart';
 import './platform/audio_recorder_platform.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 enum RecorderState { start, continuing, stop }
 
@@ -17,11 +19,33 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Builder(
+        builder: (context) {
+          final locale = Localizations.localeOf(context);
+          return MyHome(language: locale.languageCode);
+        },
+      ),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  final String language;
+  const MyHome({super.key, required this.language});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
 }
 
 const registerSuccess = "registerSuccess";
@@ -30,23 +54,21 @@ const registerWithDifferentAddress = "registerWithDifferentAddress";
 const serverError = "serverError";
 const postError = "postError";
 
-class _MyAppState extends State<MyApp> with AudioRecorderMixin, SaveAudioMixin {
+class _MyHomeState extends State<MyHome>
+    with AudioRecorderMixin, SaveAudioMixin {
   bool showPlayer = false;
   String? audioPath;
   final addressController = TextEditingController();
   String uploadResult = '';
   late String? _apiUrl = '';
-  late String? _text = '0123456789';
+  String _language = 'en';
 
   @override
   void initState() {
     super.initState();
     showPlayer = false;
     _apiUrl = dotenv.env['API_URL'];
-    final text = dotenv.env['TEXT'];
-    if (text != null) {
-      _text = text;
-    }
+    _language = widget.language;
   }
 
   @override
@@ -93,101 +115,171 @@ class _MyAppState extends State<MyApp> with AudioRecorderMixin, SaveAudioMixin {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            color: Colors.grey[200],
-            padding: const EdgeInsets.all(20),
-            width: 800,
-            child: SizedBox(
-              height: 600,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center, // 添加这一行
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('请输入你的地址：', style: TextStyle(fontSize: 16)),
-                      const SizedBox(height: 10),
-                      SizedBox(
-                        width: 500,
-                        height: 40,
-                        child: TextField(
-                          // 添加这个输入框
-                          controller: addressController,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: '地址',
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.grey[200],
+          padding: const EdgeInsets.all(20),
+          width: 800,
+          child: SizedBox(
+            height: 600,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    MenuAnchor(
+                      builder: (context, controller, child) {
+                        return FilledButton.tonal(
+                          onPressed: () {
+                            if (controller.isOpen) {
+                              controller.close();
+                            } else {
+                              controller.open();
+                            }
+                          },
+                          child: Text(
+                              AppLocalizations.of(context)!.selectLanguage),
+                        );
+                      },
+                      menuChildren: [
+                        MenuItemButton(
+                          child: Row(
+                            children: [
+                              Flag.fromCode(
+                                FlagsCode.US,
+                                height: 20,
+                                width: 20,
+                              ),
+                              const SizedBox(width: 10),
+                              const Text('English '),
+                            ],
                           ),
+                          onPressed: () {
+                            setState(() {
+                              _language = 'en';
+                            });
+                          },
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                      const Text('请朗读红色文字并录音: ',
-                          style: TextStyle(fontSize: 16)),
-                      const SizedBox(height: 10),
-                      Text(_text!,
-                          style:
-                              const TextStyle(color: Colors.red, fontSize: 24)),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 32,
-                  ),
-                  Recorder(
-                    onStart: () {
-                      setState(() {
-                        showPlayer = false;
-                      });
-                    },
-                    onReadStream: (data) {},
-                    onStop: (path) {
-                      if (kDebugMode) print('Recorded file path: $path');
-                      if (path == null) {
-                      } else {
-                        // file
-                        audioPath = path;
-                        setState(() {
-                          showPlayer = true;
-                        });
-                      }
-                    },
-                  ),
-                  showPlayer
-                      ? Column(
-                          children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 25),
-                              child: AudioPlayer(
-                                source: audioPath!,
-                                onDelete: () {
-                                  setState(() => showPlayer = false);
-                                },
+                        MenuItemButton(
+                          child: Row(
+                            children: [
+                              Flag.fromCode(
+                                FlagsCode.CN,
+                                height: 20,
+                                width: 20,
+                              ),
+                              const SizedBox(width: 10),
+                              const Text('中文 '),
+                            ],
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _language = 'zh';
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                Localizations.override(
+                  context: context,
+                  locale: Locale(_language),
+                  child: Builder(
+                    builder: (context) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center, // 添加这一行
+                        children: [
+                          Text(AppLocalizations.of(context)!.inputYourAddress,
+                              style: const TextStyle(fontSize: 16)),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: 500,
+                            height: 40,
+                            child: TextField(
+                              // 添加这个输入框
+                              controller: addressController,
+                              decoration: InputDecoration(
+                                border: const OutlineInputBorder(),
+                                labelText:
+                                    AppLocalizations.of(context)!.address,
                               ),
                             ),
-                            const SizedBox(height: 20),
-                            ElevatedButton(
-                              onPressed: () async {
-                                final buffer = await getFileData(audioPath!);
-                                final result = await _uploadAudio(
-                                    addressController.text, buffer);
-                                setState(() {
-                                  uploadResult = result;
-                                });
+                          ),
+                          const SizedBox(height: 40),
+                          Text(
+                              AppLocalizations.of(context)!
+                                  .readRedTextAndRecord,
+                              style: const TextStyle(fontSize: 16)),
+                          const SizedBox(height: 20),
+                          Text(AppLocalizations.of(context)!.redText,
+                              style: const TextStyle(
+                                  color: Colors.red, fontSize: 24)),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Recorder(
+                  onStart: () {
+                    setState(() {
+                      showPlayer = false;
+                    });
+                  },
+                  onReadStream: (data) {},
+                  onStop: (path) {
+                    if (kDebugMode) print('Recorded file path: $path');
+                    if (path == null) {
+                    } else {
+                      // file
+                      audioPath = path;
+                      setState(() {
+                        showPlayer = true;
+                      });
+                    }
+                  },
+                ),
+                showPlayer
+                    ? Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 25),
+                            child: AudioPlayer(
+                              source: audioPath!,
+                              onDelete: () {
+                                setState(() => showPlayer = false);
                               },
-                              child: const Text('发送录音'),
                             ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            Text(uploadResult),
-                          ],
-                        )
-                      : Container(),
-                ],
-              ),
+                          ),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () async {
+                              final buffer = await getFileData(audioPath!);
+                              final result = await _uploadAudio(
+                                  addressController.text, buffer);
+                              setState(() {
+                                uploadResult = result;
+                              });
+                            },
+                            child: Text(
+                                AppLocalizations.of(context)!.identifySound),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(uploadResult),
+                        ],
+                      )
+                    : Container(),
+              ],
             ),
           ),
         ),
